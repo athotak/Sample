7cd9a8d8ec6c7bb7ebc74d5c70a839b047a25504
AnilThota <anilkumar.thota@gmail.com>
2020-02-24 22:41:47 +0530

    Added Hello World Sample program

--- Sample.java
+++ Sample.java
@@ -6,6 +6,6 @@ public class Sample
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World with Jenkins!" );
     }
 }
